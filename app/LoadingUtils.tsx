f42abda40950275e1380c69618e762ab55997bcc
ultrafro <hisham.bedri@gmail.com>
2024-03-04 02:04:29 -0500

    fix spine for ios

--- app/LoadingUtils.tsx
+++ app/LoadingUtils.tsx
@@ -30,34 +30,54 @@ export async function LoadTexture(path: string): Promise<{
   error?: any;
 }> {
   console.log("loading texture at path: ", path);
+  let texture: THREE.Texture | null = null;
   try {
-    const RSResult = await getRemoteSource(
-      path,
-      undefined,
-      false,
-      false,
-      undefined
-    );
-    if (!!RSResult?.contents) {
-      let texture: THREE.Texture | null = null;
-      try {
-        // texture = await ExpoTHREE.loadAsync(RSResult.localFile);
-        texture = await ExpoTHREE.loadAsync(path);
-      } catch (e) {
-        console.error("Error loading texture: ", e);
-      }
-
-      return {
-        texture,
-        width: texture.image.width,
-        height: texture.image.height,
-      };
-    } else {
-      return { error: "No contents found in remote source result: " + path };
-    }
-  } catch (error) {
-    return { error };
+    // texture = await ExpoTHREE.loadAsync(RSResult.localFile);
+    texture = await ExpoTHREE.loadAsync(path);
+  } catch (e) {
+    console.error("Error loading texture: ", e);
+    return { error: e };
   }
+
+  console.log("loaded texture.", texture.image.width, texture.image.height);
+
+  return {
+    texture,
+    width: texture.image.width,
+    height: texture.image.height,
+  };
+
+  // try {
+  //   const RSResult = await getRemoteSource(
+  //     path,
+  //     undefined,
+  //     false,
+  //     false,
+  //     undefined
+  //   );
+  //   if (!!RSResult?.contents) {
+  //     let texture: THREE.Texture | null = null;
+  //     console.log("TRYING TO LOAD TEXTURE");
+  //     try {
+  //       // texture = await ExpoTHREE.loadAsync(RSResult.localFile);
+  //       texture = await ExpoTHREE.loadAsync(path);
+  //     } catch (e) {
+  //       console.error("Error loading texture: ", e);
+  //     }
+
+  //     console.log("loaded texture.", texture.image.width, texture.image.height);
+
+  //     return {
+  //       texture,
+  //       width: texture.image.width,
+  //       height: texture.image.height,
+  //     };
+  //   } else {
+  //     return { error: "No contents found in remote source result: " + path };
+  //   }
+  // } catch (error) {
+  //   return { error };
+  // }
 }
 
 export function LoadTextWithCallback(
